6320b09b0a75d480a32aa8122b19fa7325bf4879
nicoxeye <nicoxeye@op.pl>
2025-04-12 11:57:04 +0200

    paddle movements + structures
    structures for better organisation, enemy + player paddle, methods

--- main.c
+++ main.c
@@ -1,20 +1,114 @@
 #include "raylib.h"
 
+const int screenWidth = 800;
+const int screenHeight = 600;
+
+typedef struct {
+    float ballX;
+    float ballY;
+    int ball_speed_x;
+    int ball_speed_y;
+    int ball_radius;
+} Ball;
+
+
+void draw_ball(Ball ball){
+    DrawCircle(ball.ballX, ball.ballY, ball.ball_radius, WHITE);
+}
+
+
+void ball_movement(Ball *ball){
+    ball->ballX += ball->ball_speed_x;
+    ball->ballY += ball->ball_speed_y;
+
+    // "bouncing of edges", changing direction of the ball if met with the edge of the screen
+    if (ball->ballX + ball->ball_radius >= screenWidth || ball->ballX - ball->ball_radius <= 0) {
+        ball->ball_speed_x *= -1;
+    }
+
+    if (ball->ballY + ball->ball_radius >= screenHeight || ball->ballY - ball->ball_radius <= 0) {
+        ball->ball_speed_y *= -1;
+    }
+}
+
+
+typedef struct {
+    float x;
+    float y;
+    float width;
+    float height;
+    int speed;
+} Paddle;
+
+
+void draw_paddle(Paddle paddle){
+    DrawRectangle(paddle.x, paddle.y, paddle.width, paddle.height, WHITE);
+}
+
+void player_paddle_movement(Paddle *paddle){
+
+    if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)){
+        paddle->y -= paddle->speed;
+    } else if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)){
+        paddle->y += paddle->speed;
+    }
+
+
+    // stopping ability to go off screen
+    if (paddle->y >= screenHeight - paddle->height){
+        paddle->y = screenHeight - paddle->height;
+    } else if (paddle->y <= 0){
+        paddle->y = 0;
+    }
+}
+
+void enemy_paddle_movement(Paddle *paddle, Ball *ball){
+    if (paddle->y + paddle->height / 2 > ball->ballY){
+        paddle->y = paddle->y - paddle->speed;
+    } 
+    else if (paddle->y + paddle->height / 2 <= ball->ballY){
+        paddle->y = paddle->y + paddle->speed;
+    }
+
+    // stopping ability to go off screen
+    if (paddle->y >= screenHeight - paddle->height){
+        paddle->y = screenHeight - paddle->height;
+    } else if (paddle->y <= 0){
+        paddle->y = 0;
+    }
+}
+
+
+Ball ball;
+Paddle player;
+Paddle enemy;
+
+
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 600;
 
     InitWindow(screenWidth, screenHeight, "PING PONG");
 
     SetTargetFPS(60);
 
     // variables
-    int ballX = 100;
-    int ballY = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 10;
+    ball.ballX = 100;
+    ball.ballY = 100;
+    ball.ball_speed_x = 5;
+    ball.ball_speed_y = 5;
+    ball.ball_radius = 10;
+
+    player.x = screenWidth - player.width - 35;
+    player.y = screenHeight / 2 - player.height / 2;
+    player.width = 25;
+    player.height = 120;
+    player.speed = 6;
+
+    enemy.x = 10;
+    enemy.y = screenHeight / 2 - 50;
+    enemy.width = 25;
+    enemy.height = 120;
+    enemy.speed = 6;
 
     while (!WindowShouldClose())
     {
@@ -27,23 +121,19 @@ int main(void)
         // draw
         BeginDrawing();
 
-            ClearBackground(BLACK);
+            ClearBackground(VIOLET);
+            DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
 
-            ballX += ball_speed_x;
-            ballY += ball_speed_y;
+            draw_ball(ball);
+            ball_movement(&ball);
 
-            if (ballX + ball_radius >= screenWidth || ballX - ball_radius <= 0){
-                ball_speed_x *= -1;
-            }
+            // enemy
+            draw_paddle(enemy);
+            enemy_paddle_movement(&enemy, &ball);
 
-            if (ballY + ball_radius >= screenHeight || ballY - ball_radius <= 0){
-                ball_speed_y *= -1;
-            }
-        
-        DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
-        DrawCircle(ballX, ballY, ball_radius, WHITE);
-        DrawRectangle(10, screenHeight / 2 - 50, 20, 100, WHITE);
-        DrawRectangle(screenWidth - 30, screenHeight / 2 - 50, 20, 100, WHITE);
+            // player
+            draw_paddle(player);
+            player_paddle_movement(&player);
 
         EndDrawing();
     }
